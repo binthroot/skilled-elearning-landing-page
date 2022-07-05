4614332cc30aadb062075b92e2fb877ade1f5a16
Ben Thrasher <ben.r.thrasher@gmail.com>
2022-07-05 17:50:58 -0400

    page content, tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./starter-code/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        gradientBg: {
+          800: '#F2F2F2',
+        },
+        buttonHoverIndigio: {
+          800: '#666CA3',
+        },
+        darkIndigo: {
+          800: '#13183F',
+        },
+        textGray: {
+          800: '#83869A',
+        },
+        textPink: {
+          800: '#F74780',
+        },
+        textPinkLink: {
+          800: '#FFA7C3',
+        },
+        gradientPink: {
+          800: '#F02AA6',
+          100: '#FF6F48',
+        },
+        gradientPurple: {
+          800: '#4851FF',
+          100: '#F02AA6',
+        },
+        courseBgGradient: {
+          800: '#f0f1ff',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
